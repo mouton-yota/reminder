5747455590f7c8771fb2281a5c3ab721b9adb244
vagrant <vagrant@localhost.localdomain>
2020-11-21 05:16:34 +0000

    adduserid

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_083811) do
+ActiveRecord::Schema.define(version: 2020_11_21_050843) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
